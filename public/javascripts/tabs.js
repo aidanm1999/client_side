5c1bb5b9bfe0a5496647b9f8a49bf7b7dedd24a2
Aidan Marshall <aidanmarshall1999@gmail.com>
2019-12-02 16:23:42 +0000

    Dark mode button changed to icon
    Signed-off-by: Aidan Marshall <aidanmarshall1999@gmail.com>

--- public/javascripts/tabs.js
+++ public/javascripts/tabs.js
@@ -14,15 +14,13 @@ function openTab(evt, tabName) {
 
 
 function darkMode() {
-  var darkMode = $('#dark-mode-button').text();
-  if (darkMode === "Disable Dark Mode") {
+  var darkMode = $('body').attr("class");
+  if (darkMode === "body-dark-mode") {
     $("body").removeClass();
-    $("#dark-mode-button").html("Enable Dark Mode");
     $(".tabcontent").removeClass('tab-dark-mode');
     $("table").removeClass("table-dark");
   } else {
     $("body").addClass("body-dark-mode");
-    $("#dark-mode-button").html("Disable Dark Mode");
     $(".tabcontent").addClass("tab-dark-mode");
     $("table").addClass("table-dark");
   }
